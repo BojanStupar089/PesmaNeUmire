44cd78ddb2c0844e7abbb6921615d5b9de9c33f6
BojanStupar089 <bojanstupar089@gmail.com>
2025-10-03 23:44:58 +0200

    Add project files.

---
+++ PesmaNeUmire/Controllers/ZvezdaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PesmaNeUmire.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ZvezdaController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Sampioni EVROPE I SVETA 91");
+        }
+    }
+}
